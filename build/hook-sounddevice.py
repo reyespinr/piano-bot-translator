ce85e21cab03fead07b63f1a798210a7bd9eaa8a
Nathan Reyes <reyespinr@vcu.edu>
2025-06-05 01:52:06 -0400

    Refactor project structure: update .gitignore, remove unused build scripts, and enhance requirements.txt for better dependency management

--- build/hook-sounddevice.py
+++
@@ -1,8 +0,0 @@
-import os
-import importlib.util
-            
-spec = importlib.util.find_spec('sounddevice')
-site_packages = os.path.dirname(spec.origin)
-binary_folder = os.path.join("_sounddevice_data", "portaudio-binaries")
-
-datas = [(os.path.join(site_packages, binary_folder), binary_folder)]
